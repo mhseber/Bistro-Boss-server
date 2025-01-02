e51d671312458591c4da389bbf2b5594e7ca486f
mahamudul hasan seber <sebermh@gmail.com>
2025-01-03 01:39:21 +0600

    added code

--- index.js
+++ index.js
@@ -44,13 +44,22 @@ async function run() {
 
         //carts collection
         app.get('/carts', async (req, res) => {
-            const result = await cartCollection.find().toArray();
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
             res.send(result);
         });
 
         app.post('/carts', async (req, res) => {
             const cartItem = req.body;
             const result = await cartCollection.insertOne(cartItem);
+            res.send(result);
+        });
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new Object(id) }
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         });
 
