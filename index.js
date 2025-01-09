0b74a3a3c20dc17a6e2821b80a154ae81d4685c3
mahamudul hasan seber <sebermh@gmail.com>
2025-01-09 13:46:38 +0600

    added code

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ async function run() {
         const menuCollection = client.db("BistroDb").collection("menu");
         const reviewCollection = client.db("BistroDb").collection("reviews");
         const cartCollection = client.db("BistroDb").collection("carts");
+        const paymentCollection = client.db("BistroDb").collection("payments");
 
 
         // jwt related api
@@ -213,6 +214,30 @@ async function run() {
             })
         });
 
+        app.get('/payments/:email', verifyToken, async (req, res) => {
+            const query = { email: req.params.email }
+            if (req.params.email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            const result = await paymentCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const paymentResult = await paymentCollection.insertOne(payment);
+
+            //carefully delete each item from the cart
+            console.log('payment info', payment)
+            const query = {
+                _id: {
+                    $in: payment.cartIds.map(id => new ObjectId(id))
+                }
+            };
+
+            const deleteResult = await cartCollection.deleteMany(query);
+            res.send({ paymentResult, deleteResult });
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
